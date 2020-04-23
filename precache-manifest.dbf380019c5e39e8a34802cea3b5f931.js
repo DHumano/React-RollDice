046976c1be6a3bcff8bf2c467a32d89cbd18e6ba
dario <darioh.dev@gmail.com>
2020-04-23 14:10:20 -0300

    Updates

---
+++ precache-manifest.dbf380019c5e39e8a34802cea3b5f931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a577b43e15ae723483347127a3ee6c1",
+    "url": "/React-RollDice/index.html"
+  },
+  {
+    "revision": "83a03e6d675c38a11a1b",
+    "url": "/React-RollDice/static/css/main.a8cb013e.chunk.css"
+  },
+  {
+    "revision": "ae8186947d48e27d74a7",
+    "url": "/React-RollDice/static/js/2.73e1e0ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-RollDice/static/js/2.73e1e0ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a03e6d675c38a11a1b",
+    "url": "/React-RollDice/static/js/main.49d02f24.chunk.js"
+  },
+  {
+    "revision": "428ee8e795f82bcea285",
+    "url": "/React-RollDice/static/js/runtime-main.e34ced57.js"
+  }
+]);
